facbbd9f75a02a2e58e71afcd74b014b5efff17c
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-06-14 08:36:35 +0300

    Add files via upload

---
+++ C#/ICPC-Assiut-University-Community/Sheet1/B.BasicDataTypes/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(" ");
+            int val1 = int.Parse(arr[0]);
+            long val2 = long.Parse(arr[1]);
+            char var3 = char.Parse(arr[2]);
+            float var4 = float.Parse(arr[3]);
+            double var5 = double.Parse(arr[4]);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+        }
+    }
+}
