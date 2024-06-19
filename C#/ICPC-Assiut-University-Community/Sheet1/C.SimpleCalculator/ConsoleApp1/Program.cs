4226a0ed83f8d39eb294362ea713089ba9d7a038
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-06-20 01:13:23 +0300

    Add files via upload

---
+++ C#/ICPC-Assiut-University-Community/Sheet1/C.SimpleCalculator/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(" ");
+            long val1 = long.Parse(arr[0]);
+            long val2 = long.Parse(arr[1]);
+            Console.WriteLine($"{val1} + {val2} = {val1 + val2}");
+            Console.WriteLine($"{val1} * {val2} = {val1 * val2}");
+            Console.WriteLine($"{val1} - {val2} = {val1 - val2}");
+
+        }
+    }
+}
