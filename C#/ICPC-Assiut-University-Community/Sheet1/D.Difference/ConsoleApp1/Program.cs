35a67de49d409c310bf80cd9166f4b4d8f5cfefb
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-06-21 11:54:10 +0300

    Add files via upload

---
+++ C#/ICPC-Assiut-University-Community/Sheet1/D.Difference/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(" ");
+            long val1 = long.Parse(arr[0]);
+            long val2 = long.Parse(arr[1]);
+            long val3 = long.Parse(arr[2]);
+            long val4 = long.Parse(arr[3]);
+
+            long result = ((val1 * val2) - (val3 * val4));
+            Console.WriteLine("Difference = " + result);
+
+        }
+    }
+}
