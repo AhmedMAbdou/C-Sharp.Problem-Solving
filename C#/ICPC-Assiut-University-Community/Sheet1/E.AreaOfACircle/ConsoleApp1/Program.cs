0ad45cbfce36da4da288f1eb6e2fda7e9f20af5d
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-06-22 09:21:59 +0300

    Add files via upload

---
+++ C#/ICPC-Assiut-University-Community/Sheet1/E.AreaOfACircle/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double p = 3.141592653;
+            double input = double.Parse(Console.ReadLine());
+            double result = p * Math.Pow(input, 2);
+            Console.WriteLine(result);
+
+        }
+    }
+}
