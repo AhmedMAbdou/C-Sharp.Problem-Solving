89182534a3ce61f48df47647aade40a6eaa38c93
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-06-26 01:41:06 +0300

    Add files via upload

---
+++ C#/ICPC-Assiut-University-Community/Sheet1/G.SummationFrom1ToN/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger input = BigInteger.Parse(Console.ReadLine());
+            BigInteger result = 0;
+            for (BigInteger i = 1; i <= input; i++)
+            {
+                result = result + i;
+            }
+            Console.WriteLine(result);
+
+        }
+    }
+}
