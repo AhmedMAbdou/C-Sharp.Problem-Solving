6dd9ced6cd6b87d9f02a3028a11a28b41093eef2
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-06-25 03:21:28 +0300

    Add files via upload

---
+++ C#/ICPC-Assiut-University-Community/Sheet1/F.DigitsSummation/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(" ");
+            string val1 = arr[0][(arr[0].Length) - 1].ToString();
+            string val2 = arr[1][(arr[1].Length) - 1].ToString();
+
+            Console.WriteLine(Convert.ToInt32(val1) + Convert.ToInt32(val2));
+
+        }
+    }
+}
