d2ac2016f3f5d370b8634fd628bace98f14e9355
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-07-08 05:15:20 +0300

    Add files via upload

---
+++ C#/ICPC-Assiut-University-Community/Sheet1/OtherProblems/A.WinterSale/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(" ");
+            float num1 = long.Parse(arr[0]);
+            float num2 = long.Parse(arr[1]);
+            float result = num2 / ((100-num1)/100);
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ C#/ICPC-Assiut-University-Community/Sheet1/OtherProblems/B.MemoAndMomo/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(" ");
+            long a = long.Parse(arr[0]);
+            long b = long.Parse(arr[1]);
+            long k = long.Parse(arr[2]);
+
+            if (a % k == 0 & b % k == 0)
+                Console.WriteLine("Both");
+            else if (a % k == 0 & b % k != 0)
+                Console.WriteLine("Memo");
+            else if (b % k == 0 & a % k != 0)
+                Console.WriteLine("Momo");
+            else if (a % k != 0 & b % k != 0)
+                Console.WriteLine("No One");
+        }
+    }
+}
