b75caf46048b91a4c2bbcfb622bb215c19e91d4b
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-07-10 23:19:45 +0300

    Add files via upload

---
+++ C#/ICPC-Assiut-University-Community/Sheet1/J.Multiples/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(" ");
+
+            long A = long.Parse(arr[0]);
+            long B = long.Parse(arr[1]);
+            if (A % B == 0 || B % A == 0)
+                Console.WriteLine("Multiples");
+            else Console.WriteLine("No Multiples");
+
+        }
+    }
+}
