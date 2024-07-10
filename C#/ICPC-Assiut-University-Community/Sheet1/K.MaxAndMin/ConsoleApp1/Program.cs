43ec7ad3a1f09859dceb60530a21e0c4d61b9c0e
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-07-10 23:12:33 +0300

    Add files via upload

---
+++ C#/ICPC-Assiut-University-Community/Sheet1/K.MaxAndMin/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(" ");
+            long small = long.MaxValue;
+            long big = long.MinValue;
+
+            for (long i = 0; i < arr.Length; i++)
+            {
+                if (Convert.ToInt32(arr[i]) < small)
+                {
+                    long smallestnum = Convert.ToInt32(arr[i]);
+                    small = smallestnum;
+                }
+                if (Convert.ToInt32(arr[i]) > big)
+                {
+                    long bigestnum = Convert.ToInt32(arr[i]);
+                    big = bigestnum;
+                }
+            }
+            Console.WriteLine($"{small} {big}");
+
+        }
+    }
+}
