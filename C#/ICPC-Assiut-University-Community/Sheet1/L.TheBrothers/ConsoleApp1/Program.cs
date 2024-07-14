8332d9ad6b926fb3c9322e23fdf1023a3d7608e1
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-07-14 05:45:05 +0300

    Add files via upload

---
+++ C#/ICPC-Assiut-University-Community/Sheet1/L.TheBrothers/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(" ");
+            string[] arr2 = Console.ReadLine().Split(" ");
+            for (long i = 1; i < arr.Length; i = i + 2)
+            {
+                if (arr[i] == arr2[i])
+                {
+                    Console.WriteLine("ARE Brothers");
+                }
+                else
+                    Console.WriteLine("NOT");
+            }
+
+        }
+    }
+}
