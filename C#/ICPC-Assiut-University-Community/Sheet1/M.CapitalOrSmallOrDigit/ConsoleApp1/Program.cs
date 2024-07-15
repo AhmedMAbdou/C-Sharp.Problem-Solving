176afe447eb1b6d5abf4ed4ddf74447330903b4b
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-07-15 06:23:06 +0300

    Add files via upload

---
+++ C#/ICPC-Assiut-University-Community/Sheet1/M.CapitalOrSmallOrDigit/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            long input2 = (int)input[0];
+            if (input2 >= 48 && input2 <= 57)
+            {
+                Console.WriteLine("IS DIGIT");
+            }
+            else if (input2 >= 65 && input2 <= 90)
+            {
+                Console.WriteLine("ALPHA\nIS CAPITAL");
+            }
+            else if (input2 >= 97 && input2 <= 122)
+            {
+                Console.WriteLine("ALPHA\r\nIS SMALL");
+            }
+
+        }
+    }
+}
