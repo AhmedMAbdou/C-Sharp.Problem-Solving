89eec7c4f9c37d37aa94b5222a644d6331c70dde
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-07-18 04:47:21 +0300

    Add files via upload

---
+++ C#/ICPC-Assiut-University-Community/Sheet1/N.char/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char input = char.Parse(Console.ReadLine());
+            if (char.IsUpper(input))
+            {
+                long upper = (int)input;
+                long upper2 = upper + 32;
+                Console.WriteLine(((char)upper2));
+            }
+            else if (char.IsLower(input))
+            {
+                long lower = (int)input;
+                long lower2 = lower - 32;
+                Console.WriteLine(((char)lower2));
+            }
+
+        }
+    }
+}
