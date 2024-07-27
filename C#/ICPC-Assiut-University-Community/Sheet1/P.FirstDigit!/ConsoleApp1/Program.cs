8401b86e04c75188ba50f1745f854485466eb4d5
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-07-28 00:57:13 +0300

    Add files via upload

---
+++ C#/ICPC-Assiut-University-Community/Sheet1/P.FirstDigit!/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = (Console.ReadLine());
+            if (input[0] % 2 == 0)
+            {
+                Console.WriteLine("EVEN");
+            }
+            else
+            {
+                Console.WriteLine("ODD");
+            }
+        }
+    }
+}
