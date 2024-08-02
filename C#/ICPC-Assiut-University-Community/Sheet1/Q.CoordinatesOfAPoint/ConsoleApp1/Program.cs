e73910145b6903d251b6a19b7475d0b54449c248
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-08-03 00:02:59 +0300

    Add files via upload

---
+++ C#/ICPC-Assiut-University-Community/Sheet1/Q.CoordinatesOfAPoint/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(" ");
+
+            double item1 = Convert.ToDouble(arr[0]);
+            double item2 = Convert.ToDouble(arr[1]);
+
+            if (item1 > 0 && item2 > 0)
+            {
+                Console.WriteLine("Q1");
+            }
+            else if (item1 > 0 && item2 < 0)
+            {
+                Console.WriteLine("Q4");
+            }
+            else if (item1 < 0 && item2 > 0)
+            {
+                Console.WriteLine("Q2");
+            }
+            else if (item1 < 0 && item2 < 0)
+            {
+                Console.WriteLine("Q3");
+            }
+            else if (item1 == 0 && item2 == 0)
+            {
+                Console.WriteLine("Origem");
+            }
+            else if ((item1 > 0 | item1 < 0) && item2 == 0)
+            {
+                Console.WriteLine("Eixo X");
+            }
+            else if ((item2 > 0 | item2 < 0) && item1 == 0)
+            {
+                Console.WriteLine("Eixo Y");
+            }
+
+        }
+    }
+}
