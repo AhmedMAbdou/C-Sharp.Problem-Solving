8beb163f563d3444670a66fd848e5ed3fcf491be
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-08-04 06:12:37 +0300

    Add files via upload

---
+++ C#/ICPC-Assiut-University-Community/Sheet1/R.AgeInDays/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int year = 365;
+            const int month = 30;
+
+            int input = int.Parse(Console.ReadLine());  //800
+
+            int yearsresult = input / 365;              //          800/365 = 2
+            int monthresult = (input - (yearsresult * year)) / 30; // (800-(2*365))/30 =2
+            int dayresult = (input - (yearsresult * year)) - (monthresult * month);                //
+
+            Console.WriteLine($"{yearsresult} years");
+            Console.WriteLine($"{monthresult} months");
+            Console.WriteLine($"{dayresult} days");
+
+        }
+    }
+}
