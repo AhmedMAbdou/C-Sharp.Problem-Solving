741df9466fcb566b50be5787de9c78c5bc03dfe1
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-08-13 23:26:07 +0300

    Add files via upload

---
+++ C#/ICPC-Assiut-University-Community/Sheet1/T.SortNumbers/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(" ");        
+
+            long[] arr2 = new long[arr.Length];
+
+            for (long i = 0; i < arr.Length; i++)
+            {
+                long ConvertToInt = Convert.ToInt32(arr[i]);
+                arr2[i] = ConvertToInt;
+            }
+            Array.Sort(arr2);
+            for (long i = 0; i < arr2.Length; i++)
+            {
+                Console.WriteLine(arr2[i]);
+            }
+            Console.WriteLine();
+            for (long i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+        }
+    }
+}
