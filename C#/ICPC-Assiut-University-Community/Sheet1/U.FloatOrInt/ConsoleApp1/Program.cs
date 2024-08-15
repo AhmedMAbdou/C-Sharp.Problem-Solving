bedd7a3bf0eb676e1790d10abde13d44b87174e6
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-08-15 18:56:57 +0300

    Add files via upload

---
+++ C#/ICPC-Assiut-University-Community/Sheet1/U.FloatOrInt/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split(".");
+
+            if (Convert.ToInt32(arr[1]) == 000)
+            {
+                Console.WriteLine($"int {arr[0]}");
+            }
+            else if (Convert.ToInt32(arr[1]) != 000)
+            {
+                Console.WriteLine($"float {arr[0]} 0.{arr[1]}");
+            }
+
+        }
+    }
+}
