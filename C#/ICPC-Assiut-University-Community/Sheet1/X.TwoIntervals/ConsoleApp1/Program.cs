1e72f58d5f3a8bc7e8480ac0979794ebf28e96ae
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-08-28 17:49:29 +0300

    Add files via upload

---
+++ C#/ICPC-Assiut-University-Community/Sheet1/X.TwoIntervals/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string[] arr = Console.ReadLine().Split(" ");
+                long L1 = long.Parse(arr[0]);
+                long R1 = long.Parse(arr[1]);
+                long L2 = long.Parse(arr[2]);
+                long R2 = long.Parse(arr[3]);
+
+                if ((L2 < L1 & R2 < L1) | (L2 > R1 & R2 > R1))
+                {
+                    Console.WriteLine("-1");
+                }
+                else if ((L2 <= L1 & R2 <= R1) | (L2 < L1 & R2 > R1))
+                {
+                    Console.WriteLine($"{Math.Max(L1, L2)} {Math.Min(R1, R2)}");
+                }
+                else if (L2 >= L1 & R2 <= R1)
+                {
+                    Console.WriteLine($"{Math.Max(L1, L2)} {Math.Min(R1, R2)}");
+                }
+                else if(L2 >= L1 & R2>=R1)
+                {
+                    Console.WriteLine($"{Math.Max(L1, L2)} {Math.Min(R1, R2)}");
+                }
+
+            }
+            
+        }
+    }
+}
