105240ef7facac9c7f474d73219659cbbdef8aea
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-08-31 05:55:48 +0300

    Add files via upload

---
+++ C#/ICPC-Assiut-University-Community/Sheet1/Y.TheLast2Digits/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Numerics;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+
+                string[] arr = Console.ReadLine().Split(" ");
+
+                BigInteger multiplicate = 1;
+                for (long i = 0; i < arr.Length; i++)
+                {
+                    multiplicate = multiplicate * Convert.ToInt64(arr[i]);
+                }
+                string s = multiplicate.ToString();
+                Console.WriteLine($"{s[s.Length - 2]}{s[s.Length - 1]}");
+
+
+
+            }
+
+        }
+    }
+}
