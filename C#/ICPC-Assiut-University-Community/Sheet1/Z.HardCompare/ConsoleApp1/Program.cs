84af3158078e1c6c8be99f1eaadd1d0d63fbc87b
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-09-08 11:46:39 +0300

    Add files via upload

---
+++ C#/ICPC-Assiut-University-Community/Sheet1/Z.HardCompare/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //BigInteger x = BigInteger.Pow(8376260,70);
+            //Console.WriteLine(x);
+            while (true)
+            {
+                string[] arr = Console.ReadLine().Split(" ");
+                BigInteger[] arr_int = Array.ConvertAll(arr, BigInteger.Parse);
+
+                BigInteger x = BigInteger.Pow(arr_int[0], (int)arr_int[1]);
+                BigInteger y = BigInteger.Pow(arr_int[2], (int)arr_int[3]);
+                if (x > y)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+
+        }
+    }
+}
