10b76237bd2f9eca36b5cd31d46e91ceebdeaa2f
Ahmed Abdou <150970091+AhmedMAbdou@users.noreply.github.com>
2024-09-20 07:20:54 +0300

    Add files via upload

---
+++ C#/ICPC-Assiut-University-Community/Sheet2/A.1ToN/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long input = long.Parse(Console.ReadLine());
+
+            for (long i = 1; i <= input; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
